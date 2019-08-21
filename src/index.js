3d6fb49a04a40f1a88584ccf117c2fe747d5bff6
Joseph Bourne <joseph.bourne@tooltwist.com>
2019-08-21 15:56:27 +0800

    Lesson 34

--- src/index.js
+++ src/index.js
@@ -1,24 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
 import faker from "faker";
+
+function getAuthor() {
+  return faker.hacker.adjective() + " " + faker.hacker.noun();
+}
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            {faker.hacker.noun()}
-          </a>
-          <div className="metadata">
-            <span className="date">Tomorrow at 4:20 PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail
+        time="Apr. 20 at 4:20 PM"
+        avatar={faker.image.avatar()}
+        author={getAuthor()}
+        text={faker.hacker.phrase()}
+      />
+      <CommentDetail
+        time="Jun. 9 at 1:23 PM"
+        avatar={faker.image.avatar()}
+        author={getAuthor()}
+        text={faker.hacker.phrase()}
+      />
+      <CommentDetail
+        time="Dec. 25 at 3:33 PM"
+        avatar={faker.image.avatar()}
+        author={getAuthor()}
+        text={faker.hacker.phrase()}
+      />
+      <CommentDetail
+        time="Today at 11:59 PM"
+        avatar={faker.image.avatar()}
+        author={getAuthor()}
+        text={faker.hacker.phrase()}
+      />
+      <CommentDetail
+        time="Tomorrow at 2:22 AM"
+        avatar={faker.image.avatar()}
+        author={getAuthor()}
+        text={faker.hacker.phrase()}
+      />
     </div>
   );
 };
