577e0a4002ca39f44246fbd8303ecc9ff034f852
Joseph Bourne <joseph.bourne@tooltwist.com>
2019-08-23 16:33:32 +0800

    Lesson 83

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+  state = { term: "" };
+
+  onFormSubmit(ev) {
+    ev.preventDefault();
+    console.log(`Sent ${this.state.term}`);
+  }
+
   render() {
     return (
-      <div>
-        <form>
-          <input type="text" />
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label htmlFor="text-search">Image Search</label>
+            <input
+              id="text-search"
+              type="text"
+              onChange={ev => this.setState({ term: ev.target.value })}
+            />
+          </div>
         </form>
       </div>
     );
