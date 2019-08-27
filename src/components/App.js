46b1e95809d5d8ec258035d9d2cf1d5cc46da8fb
Joseph Bourne <joseph.bourne@tooltwist.com>
2019-08-27 14:48:13 +0800

    Lesson 92: Attempt to access state and encounter issue related to this object

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 export class App extends Component {
+  state = { images: [] };
+
   async onSearchSubmit(term) {
     console.log(term);
     const response = await axios.get("https://api.unsplash.com/search/photos", {
@@ -13,13 +15,15 @@ export class App extends Component {
       }
     });
 
-    console.log(response.data.results);
+    console.log(this);
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
