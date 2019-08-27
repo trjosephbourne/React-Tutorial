f9a15ea4fc115d33f2ddcc925ad301a15e02380b
Joseph Bourne <joseph.bourne@tooltwist.com>
2019-08-27 14:22:33 +0800

    Lesson 89: Install Axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 export class App extends Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar />
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,7 +5,8 @@ class SearchBar extends Component {
 
   onFormSubmit = ev => {
     ev.preventDefault();
-    console.log(`Sent ${this.state.term}`);
+
+    this.props.onSubmit(this.state.term);
   };
 
   render() {
