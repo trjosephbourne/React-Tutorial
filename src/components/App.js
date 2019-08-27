236439b9b5614b5b776db9be17ac70840a983a58
Joseph Bourne <joseph.bourne@tooltwist.com>
2019-08-27 16:57:15 +0800

    Lesson 99: Map list of elements of JSX & Destructuring

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import axios from "axios";
 import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 export class App extends Component {
   state = { images: [] };
 
   onSearchSubmit = async term => {
+    const nums = [1, 2, 3];
+    console.log(nums.map(n => n + 5));
     console.log(term);
     const response = await unsplash.get("/search/photos", {
       params: { query: term }
@@ -20,6 +22,7 @@ export class App extends Component {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
         Found: {this.state.images.length} images
       </div>
     );
