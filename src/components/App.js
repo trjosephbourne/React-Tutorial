545db47b5bfe77c29bf113f66df7c66c2bc89c0a
Joseph Bourne <joseph.bourne@tooltwist.com>
2019-08-27 14:31:49 +0800

    Lesson 90: Send search request to Unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,13 @@ import SearchBar from "./SearchBar";
 export class App extends Component {
   onSearchSubmit(term) {
     console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 3d2b6b17f0f94e40a70657dcb6884fba1ac8b6cdbda882bdf4813eaf1821f6e2"
+      }
+    });
   }
 
   render() {
