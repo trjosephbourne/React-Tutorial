921d35757ab02525f419b264c1c759f7ab6006e4
Joseph Bourne <joseph.bourne@tooltwist.com>
2019-08-28 10:06:10 +0800

    Lesson 105: Add event listener for ref images

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 
 export default class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  };
+
   render() {
     const { description, urls } = this.props.img;
 
     return (
       <div>
-        <img alt={description} src={urls.regular} />
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
