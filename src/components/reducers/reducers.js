223fa5639944da5ec556492cd15c2798af679bce
Joseph Bourne <joseph.bourne@tooltwist.com>
2019-08-29 13:59:45 +0800

    136: Added reducers

--- src/components/reducers/reducers.js
+++ src/components/reducers/reducers.js
@@ -6,7 +6,7 @@ const claimsHistory = (oldListOfClaims = [], action) => {
   return oldListOfClaims;
 };
 
-const accounting = (bagOfMoney, action) => {
+const accounting = (bagOfMoney = 100, action) => {
   if (action.type === "CREATE_CLAIM") {
     return bagOfMoney - action.cashAmountToClaim;
   } else if (action.type === "CREATE_POLICY") {
@@ -18,9 +18,9 @@ const accounting = (bagOfMoney, action) => {
 
 const policyHistory = (oldListOfPolicies = [], action) => {
   if (action.type === "CREATE_POLICY") {
-    return [...oldListOfPolicies, action.payload];
+    return [...oldListOfPolicies, action.payload.name];
   } else if (action.type === "DELETE_POLICY") {
-    return oldListOfPolicies;
+    return oldListOfPolicies.filter(name => name !== action.payload.name);
   }
 
   return oldListOfPolicies;
