c345c9d45e40f30f78ef3e92846d0ed63a5b79a4
Joseph Bourne <joseph.bourne@tooltwist.com>
2019-08-30 14:31:34 +0800

    Lesson 149: Select buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+import SongList from "./SongList";
 
 export default class App extends Component {
   render() {
-    return <div className="ui">App</div>;
+    return (
+      <div className="ui container grid">
+        <div className="ui row">
+          <div className="column eight wide">
+            <SongList />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
